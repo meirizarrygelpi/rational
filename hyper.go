1e7077e5784bd13119b85cb0323cb0cb04020af7
Melvin Eloy Irizarry-Gelpí <melvineloy@gmail.com>
2016-06-19 09:53:58 -0400

    Add new type: Hyper

---
+++ hyper.go
@@ -0,0 +1,224 @@
+// Copyright (c) 2016 Melvin Eloy Irizarry-Gelpí
+// Licenced under the MIT License.
+
+package rational
+
+import (
+	"fmt"
+	"math/big"
+	"math/rand"
+	"reflect"
+	"strings"
+)
+
+var symbHyper = [4]string{"", "θ", "ι", "κ"}
+
+// A Hyper represents a rational hyper-dual number.
+type Hyper struct {
+	l, r Infra
+}
+
+// Real returns the (rational) real part of z.
+func (z *Hyper) Real() *big.Rat {
+	return (&z.l).Real()
+}
+
+// Rats returns the four rational components of z.
+func (z *Hyper) Rats() (*big.Rat, *big.Rat, *big.Rat, *big.Rat) {
+	return &z.l.l, &z.l.r, &z.r.l, &z.r.r
+}
+
+// String returns the string representation of a Hyper value.
+//
+// If z corresponds to a + bθ + cι + dκ, then the string is "(a+bθ+cι+dκ)",
+// similar to complex128 values.
+func (z *Hyper) String() string {
+	v := make([]*big.Rat, 4)
+	v[0], v[1] = z.l.Rats()
+	v[2], v[3] = z.r.Rats()
+	a := make([]string, 9)
+	a[0] = "("
+	a[1] = fmt.Sprintf("%v", v[0].RatString())
+	i := 1
+	for j := 2; j < 8; j = j + 2 {
+		if v[i].Sign() == -1 {
+			a[j] = fmt.Sprintf("%v", v[i].RatString())
+		} else {
+			a[j] = fmt.Sprintf("+%v", v[i].RatString())
+		}
+		a[j+1] = symbHyper[i]
+		i++
+	}
+	a[8] = ")"
+	return strings.Join(a, "")
+}
+
+// Equals returns true if y and z are equal.
+func (z *Hyper) Equals(y *Hyper) bool {
+	if !z.l.Equals(&y.l) || !z.r.Equals(&y.r) {
+		return false
+	}
+	return true
+}
+
+// Set sets z equal to y, and returns z.
+func (z *Hyper) Set(y *Hyper) *Hyper {
+	z.l.Set(&y.l)
+	z.r.Set(&y.r)
+	return z
+}
+
+// NewHyper returns a *Hyper with value a+bθ+cι+dκ.
+func NewHyper(a, b, c, d *big.Rat) *Hyper {
+	z := new(Hyper)
+	z.l.l.Set(a)
+	z.l.r.Set(b)
+	z.r.l.Set(c)
+	z.r.r.Set(d)
+	return z
+}
+
+// Scal sets z equal to y scaled by a, and returns z.
+func (z *Hyper) Scal(y *Hyper, a *big.Rat) *Hyper {
+	z.l.Scal(&y.l, a)
+	z.r.Scal(&y.r, a)
+	return z
+}
+
+// Neg sets z equal to the negative of y, and returns z.
+func (z *Hyper) Neg(y *Hyper) *Hyper {
+	z.l.Neg(&y.l)
+	z.r.Neg(&y.r)
+	return z
+}
+
+// Conj sets z equal to the conjugate of y, and returns z. If y is a zero
+// divisor, then Conj panics.
+func (z *Hyper) Conj(y *Hyper) *Hyper {
+	if y.IsZeroDiv() {
+		panic("conjugate of zero divisor")
+	}
+	a, b, c, d := y.Rats()
+	z.l.l.Set(a)
+	z.l.r.Neg(b)
+	z.r.l.Neg(c)
+	temp := new(big.Rat)
+	temp.Mul(b, c)
+	temp.Add(temp, temp)
+	temp.Quo(temp, a)
+	temp.Sub(temp, d)
+	z.r.r.Set(temp)
+	return z
+}
+
+// Add sets z equal to x+y, and returns z.
+func (z *Hyper) Add(x, y *Hyper) *Hyper {
+	z.l.Add(&x.l, &y.l)
+	z.r.Add(&x.r, &y.r)
+	return z
+}
+
+// Sub sets z equal to x-y, and returns z.
+func (z *Hyper) Sub(x, y *Hyper) *Hyper {
+	z.l.Sub(&x.l, &y.l)
+	z.r.Sub(&x.r, &y.r)
+	return z
+}
+
+// Mul sets z equal to the product of x and y, and returns z.
+//
+// The multiplication rules are:
+// 		Mul(θ, θ) = Mul(ι, ι) = Mul(κ, κ) = 0
+// 		Mul(θ, ι) = Mul(ι, θ) = κ
+// 		Mul(ι, κ) = Mul(κ, ι) = 0
+// 		Mul(κ, θ) = Mul(θ, ι) = 0
+// This binary operation is commutative and associative.
+func (z *Hyper) Mul(x, y *Hyper) *Hyper {
+	a := new(Infra).Set(&x.l)
+	b := new(Infra).Set(&x.r)
+	c := new(Infra).Set(&y.l)
+	d := new(Infra).Set(&y.r)
+	temp := new(Infra)
+	z.l.Mul(a, c)
+	z.r.Add(
+		z.r.Mul(a, d),
+		temp.Mul(b, c),
+	)
+	return z
+}
+
+// Quad returns the quadrance of z. If z = a+bθ+cι+dκ, then the quadrance is
+// 		Mul(a, a)
+// This is always non-negative.
+func (z *Hyper) Quad() *big.Rat {
+	return z.l.Quad()
+}
+
+// IsZeroDiv returns true if z is a zero divisor.
+func (z *Hyper) IsZeroDiv() bool {
+	return z.l.IsZeroDiv()
+}
+
+// Inv sets z equal to the inverse of y, and returns z. If y is a zero divisor,
+// then Inv panics.
+func (z *Hyper) Inv(y *Hyper) *Hyper {
+	if y.IsZeroDiv() {
+		panic("inverse of zero divisor")
+	}
+	quad := y.Quad()
+	quad.Inv(quad)
+	return z.Scal(z.Conj(y), quad)
+}
+
+// Quo sets z equal to the quotient of x and y. If y is a zero divisor, then
+// Quo panics.
+func (z *Hyper) Quo(x, y *Hyper) *Hyper {
+	if y.IsZeroDiv() {
+		panic("denominator is zero divisor")
+	}
+	return z.Mul(z.Inv(y), x)
+}
+
+// CrossRatio sets z equal to the cross-ratio of v, w, x, and y:
+// 		Inv(w - x) * (v - x) * Inv(v - y) * (w - y)
+// Then it returns z.
+func (z *Hyper) CrossRatio(v, w, x, y *Hyper) *Hyper {
+	temp := new(Hyper)
+	z.Sub(w, x)
+	z.Inv(z)
+	temp.Sub(v, x)
+	z.Mul(z, temp)
+	temp.Sub(v, y)
+	temp.Inv(temp)
+	z.Mul(z, temp)
+	temp.Sub(w, y)
+	return z.Mul(z, temp)
+}
+
+// Möbius sets z equal to the Möbius (fractional linear) transform of y:
+// 		(a*y + b) * Inv(c*y + d)
+// Then it returns z.
+func (z *Hyper) Möbius(y, a, b, c, d *Hyper) *Hyper {
+	z.Mul(a, y)
+	z.Add(z, b)
+	temp := new(Hyper)
+	temp.Mul(c, y)
+	temp.Add(temp, d)
+	temp.Inv(temp)
+	return z.Mul(z, temp)
+}
+
+// Generate returns a random Hyper value for quick.Check testing.
+func (z *Hyper) Generate(rand *rand.Rand, size int) reflect.Value {
+	randomHyper := &Hyper{
+		*NewInfra(
+			big.NewRat(rand.Int63(), rand.Int63()),
+			big.NewRat(rand.Int63(), rand.Int63()),
+		),
+		*NewInfra(
+			big.NewRat(rand.Int63(), rand.Int63()),
+			big.NewRat(rand.Int63(), rand.Int63()),
+		),
+	}
+	return reflect.ValueOf(randomHyper)
+}
---
+++ hyper_test.go
@@ -0,0 +1,343 @@
+// Copyright (c) 2016 Melvin Eloy Irizarry-Gelpí
+// Licenced under the MIT License.
+
+package rational
+
+import (
+	"math/big"
+	"testing"
+	"testing/quick"
+)
+
+// Commutativity
+
+func TestHyperAddCommutative(t *testing.T) {
+	f := func(x, y *Hyper) bool {
+		// t.Logf("x = %v, y = %v", x, y)
+		l := new(Hyper).Add(x, y)
+		r := new(Hyper).Add(y, x)
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestHyperMulCommutative(t *testing.T) {
+	f := func(x, y *Hyper) bool {
+		// t.Logf("x = %v, y = %v", x, y)
+		l := new(Hyper).Mul(x, y)
+		r := new(Hyper).Mul(y, x)
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestHyperNegConjCommutative(t *testing.T) {
+	f := func(x *Hyper) bool {
+		// t.Logf("x = %v", x)
+		l, r := new(Hyper), new(Hyper)
+		l.Neg(l.Conj(x))
+		r.Conj(r.Neg(x))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+// Anti-commutativity
+
+func TestHyperSubAntiCommutative(t *testing.T) {
+	f := func(x, y *Hyper) bool {
+		// t.Logf("x = %v, y = %v", x, y)
+		l, r := new(Hyper), new(Hyper)
+		l.Sub(x, y)
+		r.Sub(y, x)
+		r.Neg(r)
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+// Associativity
+
+func TestHyperAddAssociative(t *testing.T) {
+	f := func(x, y, z *Hyper) bool {
+		// t.Logf("x = %v, y = %v, z = %v", x, y, z)
+		l, r := new(Hyper), new(Hyper)
+		l.Add(l.Add(x, y), z)
+		r.Add(x, r.Add(y, z))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestHyperMulAssociative(t *testing.T) {
+	f := func(x, y, z *Hyper) bool {
+		// t.Logf("x = %v, y = %v, z = %v", x, y, z)
+		l, r := new(Hyper), new(Hyper)
+		l.Mul(l.Mul(x, y), z)
+		r.Mul(x, r.Mul(y, z))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+// Identity
+
+func TestHyperAddZero(t *testing.T) {
+	zero := new(Hyper)
+	f := func(x *Hyper) bool {
+		// t.Logf("x = %v", x)
+		l := new(Hyper).Add(x, zero)
+		return l.Equals(x)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestHyperMulOne(t *testing.T) {
+	one := &Infra{
+		l: *big.NewRat(1, 1),
+	}
+	zero := new(Infra)
+	f := func(x *Hyper) bool {
+		// t.Logf("x = %v", x)
+		l := new(Hyper).Mul(x, &Hyper{*one, *zero})
+		return l.Equals(x)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestHyperMulInvOne(t *testing.T) {
+	one := &Infra{
+		l: *big.NewRat(1, 1),
+	}
+	zero := new(Infra)
+	f := func(x *Hyper) bool {
+		// t.Logf("x = %v", x)
+		l := new(Hyper)
+		l.Mul(x, l.Inv(x))
+		return l.Equals(&Hyper{*one, *zero})
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestHyperAddNegSub(t *testing.T) {
+	f := func(x, y *Hyper) bool {
+		// t.Logf("x = %v, y = %v", x, y)
+		l, r := new(Hyper), new(Hyper)
+		l.Sub(x, y)
+		r.Add(x, r.Neg(y))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestHyperAddScalDouble(t *testing.T) {
+	f := func(x *Hyper) bool {
+		// t.Logf("x = %v", x)
+		l, r := new(Hyper), new(Hyper)
+		l.Add(x, x)
+		r.Scal(x, big.NewRat(2, 1))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+// Involutivity
+
+func TestHyperInvInvolutive(t *testing.T) {
+	f := func(x *Hyper) bool {
+		// t.Logf("x = %v", x)
+		l := new(Hyper)
+		l.Inv(l.Inv(x))
+		return l.Equals(x)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestHyperNegInvolutive(t *testing.T) {
+	f := func(x *Hyper) bool {
+		// t.Logf("x = %v", x)
+		l := new(Hyper)
+		l.Neg(l.Neg(x))
+		return l.Equals(x)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestHyperConjInvolutive(t *testing.T) {
+	f := func(x *Hyper) bool {
+		// t.Logf("x = %v", x)
+		l := new(Hyper)
+		l.Conj(l.Conj(x))
+		return l.Equals(x)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+// Anti-distributivity
+
+func TestHyperMulConjAntiDistributive(t *testing.T) {
+	f := func(x, y *Hyper) bool {
+		// t.Logf("x = %v, y = %v", x, y)
+		l, r := new(Hyper), new(Hyper)
+		l.Conj(l.Mul(x, y))
+		r.Mul(r.Conj(y), new(Hyper).Conj(x))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestHyperMulInvAntiDistributive(t *testing.T) {
+	f := func(x, y *Hyper) bool {
+		// t.Logf("x = %v, y = %v", x, y)
+		l, r := new(Hyper), new(Hyper)
+		l.Inv(l.Mul(x, y))
+		r.Mul(r.Inv(y), new(Hyper).Inv(x))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+// Distributivity
+
+func XTestHyperAddConjDistributive(t *testing.T) {
+	f := func(x, y *Hyper) bool {
+		// t.Logf("x = %v, y = %v", x, y)
+		l, r := new(Hyper), new(Hyper)
+		l.Add(x, y)
+		l.Conj(l)
+		r.Add(r.Conj(x), new(Hyper).Conj(y))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func XTestHyperSubConjDistributive(t *testing.T) {
+	f := func(x, y *Hyper) bool {
+		// t.Logf("x = %v, y = %v", x, y)
+		l, r := new(Hyper), new(Hyper)
+		l.Sub(x, y)
+		l.Conj(l)
+		r.Sub(r.Conj(x), new(Hyper).Conj(y))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestHyperAddScalDistributive(t *testing.T) {
+	f := func(x, y *Hyper) bool {
+		// t.Logf("x = %v, y = %v", x, y)
+		a := big.NewRat(2, 1)
+		l, r := new(Hyper), new(Hyper)
+		l.Scal(l.Add(x, y), a)
+		r.Add(r.Scal(x, a), new(Hyper).Scal(y, a))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestHyperSubScalDistributive(t *testing.T) {
+	f := func(x, y *Hyper) bool {
+		// t.Logf("x = %v, y = %v", x, y)
+		a := big.NewRat(2, 1)
+		l, r := new(Hyper), new(Hyper)
+		l.Scal(l.Sub(x, y), a)
+		r.Sub(r.Scal(x, a), new(Hyper).Scal(y, a))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestHyperAddMulDistributive(t *testing.T) {
+	f := func(x, y, z *Hyper) bool {
+		// t.Logf("x = %v, y = %v, z = %v", x, y, z)
+		l, r := new(Hyper), new(Hyper)
+		l.Mul(l.Add(x, y), z)
+		r.Add(r.Mul(x, z), new(Hyper).Mul(y, z))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestHyperSubMulDistributive(t *testing.T) {
+	f := func(x, y, z *Hyper) bool {
+		// t.Logf("x = %v, y = %v, z = %v", x, y, z)
+		l, r := new(Hyper), new(Hyper)
+		l.Mul(l.Sub(x, y), z)
+		r.Sub(r.Mul(x, z), new(Hyper).Mul(y, z))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+// Positivity
+
+func TestHyperQuadPositive(t *testing.T) {
+	f := func(x *Hyper) bool {
+		// t.Logf("x = %v", x)
+		return x.Quad().Sign() > 0
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+// Composition
+
+func TestHyperComposition(t *testing.T) {
+	f := func(x, y *Hyper) bool {
+		// t.Logf("x = %v, y = %v", x, y)
+		p := new(Hyper)
+		a, b := new(big.Rat), new(big.Rat)
+		p.Mul(x, y)
+		a.Set(p.Quad())
+		b.Mul(x.Quad(), y.Quad())
+		return a.Cmp(b) == 0
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
