6490a33b949e0badb07b0a789884274c7e02fb3f
Melvin Eloy Irizarry-Gelpí <melvineloy@gmail.com>
2016-06-20 08:11:56 -0400

    Add new type: BiComplex

---
+++ bicomplex.go
@@ -0,0 +1,236 @@
+// Copyright (c) 2016 Melvin Eloy Irizarry-Gelpí
+// Licenced under the MIT License.
+
+package rational
+
+import (
+	"fmt"
+	"math/big"
+	"math/rand"
+	"reflect"
+	"strings"
+)
+
+var symbBiComplex = [4]string{"", "i", "j", "s"}
+
+// A BiComplex represents a rational bicomplex number.
+type BiComplex struct {
+	l, r Complex
+}
+
+// Real returns the (rational) real part of z.
+func (z *BiComplex) Real() *big.Rat {
+	return (&z.l).Real()
+}
+
+// Rats returns the four rational components of z.
+func (z *BiComplex) Rats() (*big.Rat, *big.Rat, *big.Rat, *big.Rat) {
+	return &z.l.l, &z.l.r, &z.r.l, &z.r.r
+}
+
+// String returns the string representation of a BiComplex value.
+//
+// If z corresponds to a + bi + cj + ds, then the string is "(a+bi+cj+ds)",
+// similar to complex128 values.
+func (z *BiComplex) String() string {
+	v := make([]*big.Rat, 4)
+	v[0], v[1] = z.l.Rats()
+	v[2], v[3] = z.r.Rats()
+	a := make([]string, 9)
+	a[0] = "("
+	a[1] = fmt.Sprintf("%v", v[0].RatString())
+	i := 1
+	for j := 2; j < 8; j = j + 2 {
+		if v[i].Sign() == -1 {
+			a[j] = fmt.Sprintf("%v", v[i].RatString())
+		} else {
+			a[j] = fmt.Sprintf("+%v", v[i].RatString())
+		}
+		a[j+1] = symbBiComplex[i]
+		i++
+	}
+	a[8] = ")"
+	return strings.Join(a, "")
+}
+
+// Equals returns true if y and z are equal.
+func (z *BiComplex) Equals(y *BiComplex) bool {
+	if !z.l.Equals(&y.l) || !z.r.Equals(&y.r) {
+		return false
+	}
+	return true
+}
+
+// Set sets z equal to y, and returns z.
+func (z *BiComplex) Set(y *BiComplex) *BiComplex {
+	z.l.Set(&y.l)
+	z.r.Set(&y.r)
+	return z
+}
+
+// NewBiComplex returns a *BiComplex with value a+bh+cj+ds.
+func NewBiComplex(a, b, c, d *big.Rat) *BiComplex {
+	z := new(BiComplex)
+	z.l.l.Set(a)
+	z.l.r.Set(b)
+	z.r.l.Set(c)
+	z.r.r.Set(d)
+	return z
+}
+
+// Scal sets z equal to y scaled by a, and returns z.
+func (z *BiComplex) Scal(y *BiComplex, a *big.Rat) *BiComplex {
+	z.l.Scal(&y.l, a)
+	z.r.Scal(&y.r, a)
+	return z
+}
+
+// Neg sets z equal to the negative of y, and returns z.
+func (z *BiComplex) Neg(y *BiComplex) *BiComplex {
+	z.l.Neg(&y.l)
+	z.r.Neg(&y.r)
+	return z
+}
+
+// Conj sets z equal to the bicomplex conjugate of y, and returns z.
+func (z *BiComplex) Conj(y *BiComplex) *BiComplex {
+	z.l.Set(&y.l)
+	z.r.Neg(&y.r)
+	return z
+}
+
+// Star sets z equal to the star conjugate of y, and returns z.
+func (z *BiComplex) Star(y *BiComplex) *BiComplex {
+	z.l.Conj(&y.l)
+	z.r.Conj(&y.r)
+	return z
+}
+
+// Add sets z equal to x+y, and returns z.
+func (z *BiComplex) Add(x, y *BiComplex) *BiComplex {
+	z.l.Add(&x.l, &y.l)
+	z.r.Add(&x.r, &y.r)
+	return z
+}
+
+// Sub sets z equal to x-y, and returns z.
+func (z *BiComplex) Sub(x, y *BiComplex) *BiComplex {
+	z.l.Sub(&x.l, &y.l)
+	z.r.Sub(&x.r, &y.r)
+	return z
+}
+
+// Mul sets z equal to the product of x and y, and returns z.
+//
+// The multiplication rules are:
+// 		Mul(i, i) = Mul(j, j) = -1
+// 		Mul(s, s) = +1
+// 		Mul(i, j) = Mul(j, i) = s
+// 		Mul(j, s) = Mul(s, j) = -i
+// 		Mul(s, i) = Mul(i, s) = -j
+// This binary operation is commutative and associative.
+func (z *BiComplex) Mul(x, y *BiComplex) *BiComplex {
+	a := new(Complex).Set(&x.l)
+	b := new(Complex).Set(&x.r)
+	c := new(Complex).Set(&y.l)
+	d := new(Complex).Set(&y.r)
+	temp := new(Complex)
+	z.l.Sub(
+		z.l.Mul(a, c),
+		temp.Mul(b, d),
+	)
+	z.r.Add(
+		z.r.Mul(a, d),
+		temp.Mul(b, c),
+	)
+	return z
+}
+
+// Norm returns the complex-valued norm of z.
+func (z *BiComplex) Norm() *Complex {
+	norm := new(Complex)
+	norm.Mul(&z.l, &z.l)
+	norm.Add(norm, new(Complex).Mul(&z.r, &z.r))
+	return norm
+}
+
+// Quad returns the quadrance of z. This is always non-negative.
+func (z *BiComplex) Quad() *big.Rat {
+	return z.Norm().Quad()
+}
+
+// IsZeroDiv returns true if z is a zero divisor.
+func (z *BiComplex) IsZeroDiv() bool {
+	zero := new(Complex)
+	return zero.Equals(z.Norm())
+}
+
+// Inv sets z equal to the inverse of y, and returns z. If y is a zero divisor,
+// then Inv panics.
+func (z *BiComplex) Inv(y *BiComplex) *BiComplex {
+	if y.IsZeroDiv() {
+		panic("inverse of zero divisor")
+	}
+	p := new(BiComplex)
+	p.Set(y)
+	quad := p.Quad()
+	quad.Inv(quad)
+	temp := new(BiComplex)
+	z.Conj(p)
+	z.Mul(z, temp.Star(p))
+	z.Mul(z, temp.Conj(temp.Star(p)))
+	return z.Scal(z, quad)
+}
+
+// Quo sets z equal to the quotient of x and y. If y is a zero divisor, then
+// Quo panics.
+func (z *BiComplex) Quo(x, y *BiComplex) *BiComplex {
+	if zero := new(BiComplex); y.Equals(zero) {
+		panic("denominator is zero")
+	}
+	return z.Mul(z.Inv(y), x)
+}
+
+// CrossRatio sets z equal to the cross-ratio of v, w, x, and y:
+// 		Inv(w - x) * (v - x) * Inv(v - y) * (w - y)
+// Then it returns z.
+func (z *BiComplex) CrossRatio(v, w, x, y *BiComplex) *BiComplex {
+	temp := new(BiComplex)
+	z.Sub(w, x)
+	z.Inv(z)
+	temp.Sub(v, x)
+	z.Mul(z, temp)
+	temp.Sub(v, y)
+	temp.Inv(temp)
+	z.Mul(z, temp)
+	temp.Sub(w, y)
+	return z.Mul(z, temp)
+}
+
+// Möbius sets z equal to the Möbius (fractional linear) transform of y:
+// 		(a*y + b) * Inv(c*y + d)
+// Then it returns z.
+func (z *BiComplex) Möbius(y, a, b, c, d *BiComplex) *BiComplex {
+	z.Mul(a, y)
+	z.Add(z, b)
+	temp := new(BiComplex)
+	temp.Mul(c, y)
+	temp.Add(temp, d)
+	temp.Inv(temp)
+	return z.Mul(z, temp)
+}
+
+// Generate returns a random BiComplex value for quick.Check testing.
+func (z *BiComplex) Generate(rand *rand.Rand, size int) reflect.Value {
+	randomBiComplex := &BiComplex{
+		*NewComplex(
+			big.NewRat(rand.Int63(), rand.Int63()),
+			big.NewRat(rand.Int63(), rand.Int63()),
+		),
+		*NewComplex(
+			big.NewRat(rand.Int63(), rand.Int63()),
+			big.NewRat(rand.Int63(), rand.Int63()),
+		),
+	}
+	return reflect.ValueOf(randomBiComplex)
+}
---
+++ bicomplex_test.go
@@ -0,0 +1,343 @@
+// Copyright (c) 2016 Melvin Eloy Irizarry-Gelpí
+// Licenced under the MIT License.
+
+package rational
+
+import (
+	"math/big"
+	"testing"
+	"testing/quick"
+)
+
+// Commutativity
+
+func TestBiComplexAddCommutative(t *testing.T) {
+	f := func(x, y *BiComplex) bool {
+		// t.Logf("x = %v, y = %v", x, y)
+		l := new(BiComplex).Add(x, y)
+		r := new(BiComplex).Add(y, x)
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestBiComplexMulCommutative(t *testing.T) {
+	f := func(x, y *BiComplex) bool {
+		// t.Logf("x = %v, y = %v", x, y)
+		l := new(BiComplex).Mul(x, y)
+		r := new(BiComplex).Mul(y, x)
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestBiComplexNegConjCommutative(t *testing.T) {
+	f := func(x *BiComplex) bool {
+		// t.Logf("x = %v", x)
+		l, r := new(BiComplex), new(BiComplex)
+		l.Neg(l.Conj(x))
+		r.Conj(r.Neg(x))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+// Anti-commutativity
+
+func TestBiComplexSubAntiCommutative(t *testing.T) {
+	f := func(x, y *BiComplex) bool {
+		// t.Logf("x = %v, y = %v", x, y)
+		l, r := new(BiComplex), new(BiComplex)
+		l.Sub(x, y)
+		r.Sub(y, x)
+		r.Neg(r)
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+// Associativity
+
+func TestBiComplexAddAssociative(t *testing.T) {
+	f := func(x, y, z *BiComplex) bool {
+		// t.Logf("x = %v, y = %v, z = %v", x, y, z)
+		l, r := new(BiComplex), new(BiComplex)
+		l.Add(l.Add(x, y), z)
+		r.Add(x, r.Add(y, z))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestBiComplexMulAssociative(t *testing.T) {
+	f := func(x, y, z *BiComplex) bool {
+		// t.Logf("x = %v, y = %v, z = %v", x, y, z)
+		l, r := new(BiComplex), new(BiComplex)
+		l.Mul(l.Mul(x, y), z)
+		r.Mul(x, r.Mul(y, z))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+// Identity
+
+func TestBiComplexAddZero(t *testing.T) {
+	zero := new(BiComplex)
+	f := func(x *BiComplex) bool {
+		// t.Logf("x = %v", x)
+		l := new(BiComplex).Add(x, zero)
+		return l.Equals(x)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestBiComplexMulOne(t *testing.T) {
+	one := &Complex{
+		l: *big.NewRat(1, 1),
+	}
+	zero := new(Complex)
+	f := func(x *BiComplex) bool {
+		// t.Logf("x = %v", x)
+		l := new(BiComplex).Mul(x, &BiComplex{*one, *zero})
+		return l.Equals(x)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestBiComplexMulInvOne(t *testing.T) {
+	one := &Complex{
+		l: *big.NewRat(1, 1),
+	}
+	zero := new(Complex)
+	f := func(x *BiComplex) bool {
+		// t.Logf("x = %v", x)
+		l := new(BiComplex)
+		l.Mul(x, l.Inv(x))
+		return l.Equals(&BiComplex{*one, *zero})
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestBiComplexAddNegSub(t *testing.T) {
+	f := func(x, y *BiComplex) bool {
+		// t.Logf("x = %v, y = %v", x, y)
+		l, r := new(BiComplex), new(BiComplex)
+		l.Sub(x, y)
+		r.Add(x, r.Neg(y))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestBiComplexAddScalDouble(t *testing.T) {
+	f := func(x *BiComplex) bool {
+		// t.Logf("x = %v", x)
+		l, r := new(BiComplex), new(BiComplex)
+		l.Add(x, x)
+		r.Scal(x, big.NewRat(2, 1))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+// Involutivity
+
+func TestBiComplexInvInvolutive(t *testing.T) {
+	f := func(x *BiComplex) bool {
+		// t.Logf("x = %v", x)
+		l := new(BiComplex)
+		l.Inv(l.Inv(x))
+		return l.Equals(x)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestBiComplexNegInvolutive(t *testing.T) {
+	f := func(x *BiComplex) bool {
+		// t.Logf("x = %v", x)
+		l := new(BiComplex)
+		l.Neg(l.Neg(x))
+		return l.Equals(x)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestBiComplexConjInvolutive(t *testing.T) {
+	f := func(x *BiComplex) bool {
+		// t.Logf("x = %v", x)
+		l := new(BiComplex)
+		l.Conj(l.Conj(x))
+		return l.Equals(x)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+// Anti-distributivity
+
+func TestBiComplexMulConjAntiDistributive(t *testing.T) {
+	f := func(x, y *BiComplex) bool {
+		// t.Logf("x = %v, y = %v", x, y)
+		l, r := new(BiComplex), new(BiComplex)
+		l.Conj(l.Mul(x, y))
+		r.Mul(r.Conj(y), new(BiComplex).Conj(x))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestBiComplexMulInvAntiDistributive(t *testing.T) {
+	f := func(x, y *BiComplex) bool {
+		// t.Logf("x = %v, y = %v", x, y)
+		l, r := new(BiComplex), new(BiComplex)
+		l.Inv(l.Mul(x, y))
+		r.Mul(r.Inv(y), new(BiComplex).Inv(x))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+// Distributivity
+
+func TestBiComplexAddConjDistributive(t *testing.T) {
+	f := func(x, y *BiComplex) bool {
+		// t.Logf("x = %v, y = %v", x, y)
+		l, r := new(BiComplex), new(BiComplex)
+		l.Add(x, y)
+		l.Conj(l)
+		r.Add(r.Conj(x), new(BiComplex).Conj(y))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestBiComplexSubConjDistributive(t *testing.T) {
+	f := func(x, y *BiComplex) bool {
+		// t.Logf("x = %v, y = %v", x, y)
+		l, r := new(BiComplex), new(BiComplex)
+		l.Sub(x, y)
+		l.Conj(l)
+		r.Sub(r.Conj(x), new(BiComplex).Conj(y))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestBiComplexAddScalDistributive(t *testing.T) {
+	f := func(x, y *BiComplex) bool {
+		// t.Logf("x = %v, y = %v", x, y)
+		a := big.NewRat(2, 1)
+		l, r := new(BiComplex), new(BiComplex)
+		l.Scal(l.Add(x, y), a)
+		r.Add(r.Scal(x, a), new(BiComplex).Scal(y, a))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestBiComplexSubScalDistributive(t *testing.T) {
+	f := func(x, y *BiComplex) bool {
+		// t.Logf("x = %v, y = %v", x, y)
+		a := big.NewRat(2, 1)
+		l, r := new(BiComplex), new(BiComplex)
+		l.Scal(l.Sub(x, y), a)
+		r.Sub(r.Scal(x, a), new(BiComplex).Scal(y, a))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestBiComplexAddMulDistributive(t *testing.T) {
+	f := func(x, y, z *BiComplex) bool {
+		// t.Logf("x = %v, y = %v, z = %v", x, y, z)
+		l, r := new(BiComplex), new(BiComplex)
+		l.Mul(l.Add(x, y), z)
+		r.Add(r.Mul(x, z), new(BiComplex).Mul(y, z))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestBiComplexSubMulDistributive(t *testing.T) {
+	f := func(x, y, z *BiComplex) bool {
+		// t.Logf("x = %v, y = %v, z = %v", x, y, z)
+		l, r := new(BiComplex), new(BiComplex)
+		l.Mul(l.Sub(x, y), z)
+		r.Sub(r.Mul(x, z), new(BiComplex).Mul(y, z))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+// Positivity
+
+func TestBiComplexQuadPositive(t *testing.T) {
+	f := func(x *BiComplex) bool {
+		// t.Logf("x = %v", x)
+		return x.Quad().Sign() > 0
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+// Composition
+
+func TestBiComplexComposition(t *testing.T) {
+	f := func(x, y *BiComplex) bool {
+		// t.Logf("x = %v, y = %v", x, y)
+		p := new(BiComplex)
+		a, b := new(big.Rat), new(big.Rat)
+		p.Mul(x, y)
+		a.Set(p.Quad())
+		b.Mul(x.Quad(), y.Quad())
+		return a.Cmp(b) == 0
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
