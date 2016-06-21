aae1c534e8b0335d3ca3c1db8abe64c65d125c5f
Melvin Eloy Irizarry-Gelpí <melvineloy@gmail.com>
2016-06-20 22:08:25 -0400

    Add new type: DualPerplex

--- dualperplex.go
+++ dualperplex.go
@@ -11,28 +11,28 @@ import (
 	"strings"
 )
 
-var symbDualComplex = [4]string{"", "i", "κ", "λ"}
+var symbDualPerplex = [4]string{"", "s", "κ", "λ"}
 
-// A DualComplex represents a rational dual-complex number.
-type DualComplex struct {
-	l, r Complex
+// A DualPerplex represents a rational dual perplex number.
+type DualPerplex struct {
+	l, r Perplex
 }
 
 // Real returns the (rational) real part of z.
-func (z *DualComplex) Real() *big.Rat {
+func (z *DualPerplex) Real() *big.Rat {
 	return (&z.l).Real()
 }
 
 // Rats returns the four rational components of z.
-func (z *DualComplex) Rats() (*big.Rat, *big.Rat, *big.Rat, *big.Rat) {
+func (z *DualPerplex) Rats() (*big.Rat, *big.Rat, *big.Rat, *big.Rat) {
 	return &z.l.l, &z.l.r, &z.r.l, &z.r.r
 }
 
-// String returns the string representation of a DualComplex value.
+// String returns the string representation of a DualPerplex value.
 //
-// If z corresponds to a + bi + cκ + dλ, then the string is "(a+bi+cκ+dλ)",
+// If z corresponds to a + bs + cκ + dλ, then the string is "(a+bs+cκ+dλ)",
 // similar to complex128 values.
-func (z *DualComplex) String() string {
+func (z *DualPerplex) String() string {
 	v := make([]*big.Rat, 4)
 	v[0], v[1] = z.l.Rats()
 	v[2], v[3] = z.r.Rats()
@@ -46,7 +46,7 @@ func (z *DualComplex) String() string {
 		} else {
 			a[j] = fmt.Sprintf("+%v", v[i].RatString())
 		}
-		a[j+1] = symbDualComplex[i]
+		a[j+1] = symbDualPerplex[i]
 		i++
 	}
 	a[8] = ")"
@@ -54,7 +54,7 @@ func (z *DualComplex) String() string {
 }
 
 // Equals returns true if y and z are equal.
-func (z *DualComplex) Equals(y *DualComplex) bool {
+func (z *DualPerplex) Equals(y *DualPerplex) bool {
 	if !z.l.Equals(&y.l) || !z.r.Equals(&y.r) {
 		return false
 	}
@@ -62,15 +62,15 @@ func (z *DualComplex) Equals(y *DualComplex) bool {
 }
 
 // Set sets z equal to y, and returns z.
-func (z *DualComplex) Set(y *DualComplex) *DualComplex {
+func (z *DualPerplex) Set(y *DualPerplex) *DualPerplex {
 	z.l.Set(&y.l)
 	z.r.Set(&y.r)
 	return z
 }
 
-// NewDualComplex returns a *DualComplex with value a+bi+cκ+dλ.
-func NewDualComplex(a, b, c, d *big.Rat) *DualComplex {
-	z := new(DualComplex)
+// NewDualPerplex returns a *DualPerplex with value a+bs+cκ+dλ.
+func NewDualPerplex(a, b, c, d *big.Rat) *DualPerplex {
+	z := new(DualPerplex)
 	z.l.l.Set(a)
 	z.l.r.Set(b)
 	z.r.l.Set(c)
@@ -79,42 +79,42 @@ func NewDualComplex(a, b, c, d *big.Rat) *DualComplex {
 }
 
 // Scal sets z equal to y scaled by a, and returns z.
-func (z *DualComplex) Scal(y *DualComplex, a *big.Rat) *DualComplex {
+func (z *DualPerplex) Scal(y *DualPerplex, a *big.Rat) *DualPerplex {
 	z.l.Scal(&y.l, a)
 	z.r.Scal(&y.r, a)
 	return z
 }
 
 // Neg sets z equal to the negative of y, and returns z.
-func (z *DualComplex) Neg(y *DualComplex) *DualComplex {
+func (z *DualPerplex) Neg(y *DualPerplex) *DualPerplex {
 	z.l.Neg(&y.l)
 	z.r.Neg(&y.r)
 	return z
 }
 
-// Conj sets z equal to the dual-complex conjugate of y, and returns z.
-func (z *DualComplex) Conj(y *DualComplex) *DualComplex {
+// Conj sets z equal to the dual-perplex conjugate of y, and returns z.
+func (z *DualPerplex) Conj(y *DualPerplex) *DualPerplex {
 	z.l.Set(&y.l)
 	z.r.Neg(&y.r)
 	return z
 }
 
 // Star sets z equal to the star conjugate of y, and returns z.
-func (z *DualComplex) Star(y *DualComplex) *DualComplex {
+func (z *DualPerplex) Star(y *DualPerplex) *DualPerplex {
 	z.l.Conj(&y.l)
 	z.r.Conj(&y.r)
 	return z
 }
 
 // Add sets z equal to x+y, and returns z.
-func (z *DualComplex) Add(x, y *DualComplex) *DualComplex {
+func (z *DualPerplex) Add(x, y *DualPerplex) *DualPerplex {
 	z.l.Add(&x.l, &y.l)
 	z.r.Add(&x.r, &y.r)
 	return z
 }
 
 // Sub sets z equal to x-y, and returns z.
-func (z *DualComplex) Sub(x, y *DualComplex) *DualComplex {
+func (z *DualPerplex) Sub(x, y *DualPerplex) *DualPerplex {
 	z.l.Sub(&x.l, &y.l)
 	z.r.Sub(&x.r, &y.r)
 	return z
@@ -123,18 +123,18 @@ func (z *DualComplex) Sub(x, y *DualComplex) *DualComplex {
 // Mul sets z equal to the product of x and y, and returns z.
 //
 // The multiplication rules are:
-// 		Mul(i, i) = -1
+// 		Mul(s, s) = +1
 // 		Mul(κ, κ) = Mul(λ, λ) = 0
-// 		Mul(i, κ) = Mul(κ, i) = λ
+// 		Mul(s, κ) = Mul(κ, s) = λ
 // 		Mul(κ, λ) = Mul(λ, κ) = 0
-// 		Mul(λ, i) = Mul(i, λ) = -κ
+// 		Mul(λ, s) = Mul(s, λ) = κ
 // This binary operation is commutative and associative.
-func (z *DualComplex) Mul(x, y *DualComplex) *DualComplex {
-	a := new(Complex).Set(&x.l)
-	b := new(Complex).Set(&x.r)
-	c := new(Complex).Set(&y.l)
-	d := new(Complex).Set(&y.r)
-	temp := new(Complex)
+func (z *DualPerplex) Mul(x, y *DualPerplex) *DualPerplex {
+	a := new(Perplex).Set(&x.l)
+	b := new(Perplex).Set(&x.r)
+	c := new(Perplex).Set(&y.l)
+	d := new(Perplex).Set(&y.r)
+	temp := new(Perplex)
 	z.l.Mul(a, c)
 	z.r.Add(
 		z.r.Mul(a, d),
@@ -143,34 +143,33 @@ func (z *DualComplex) Mul(x, y *DualComplex) *DualComplex {
 	return z
 }
 
-// Norm returns the complex norm of z.
-func (z *DualComplex) Norm() *Complex {
-	norm := new(Complex)
+// Norm returns the perplex norm of z.
+func (z *DualPerplex) Norm() *Perplex {
+	norm := new(Perplex)
 	return norm.Mul(&z.l, &z.l)
 }
 
 // Quad returns the quadrance of z. This is always non-negative.
-func (z *DualComplex) Quad() *big.Rat {
+func (z *DualPerplex) Quad() *big.Rat {
 	return z.Norm().Quad()
 }
 
 // IsZeroDiv returns true if z is a zero divisor.
-func (z *DualComplex) IsZeroDiv() bool {
-	zero := new(Complex)
-	return zero.Equals(z.Norm())
+func (z *DualPerplex) IsZeroDiv() bool {
+	return z.Norm().IsZeroDiv()
 }
 
 // Inv sets z equal to the inverse of y, and returns z. If y is a zero divisor,
 // then Inv panics.
-func (z *DualComplex) Inv(y *DualComplex) *DualComplex {
+func (z *DualPerplex) Inv(y *DualPerplex) *DualPerplex {
 	if y.IsZeroDiv() {
 		panic("inverse of zero divisor")
 	}
-	p := new(DualComplex)
+	p := new(DualPerplex)
 	p.Set(y)
 	quad := p.Quad()
 	quad.Inv(quad)
-	temp := new(DualComplex)
+	temp := new(DualPerplex)
 	z.Conj(p)
 	z.Mul(z, temp.Star(p))
 	z.Mul(z, temp.Conj(temp.Star(p)))
@@ -179,7 +178,7 @@ func (z *DualComplex) Inv(y *DualComplex) *DualComplex {
 
 // Quo sets z equal to the quotient of x and y. If y is a zero divisor, then
 // Quo panics.
-func (z *DualComplex) Quo(x, y *DualComplex) *DualComplex {
+func (z *DualPerplex) Quo(x, y *DualPerplex) *DualPerplex {
 	if y.IsZeroDiv() {
 		panic("denominator is zero divisor")
 	}
@@ -189,8 +188,8 @@ func (z *DualComplex) Quo(x, y *DualComplex) *DualComplex {
 // CrossRatio sets z equal to the cross-ratio of v, w, x, and y:
 // 		Inv(w - x) * (v - x) * Inv(v - y) * (w - y)
 // Then it returns z.
-func (z *DualComplex) CrossRatio(v, w, x, y *DualComplex) *DualComplex {
-	temp := new(DualComplex)
+func (z *DualPerplex) CrossRatio(v, w, x, y *DualPerplex) *DualPerplex {
+	temp := new(DualPerplex)
 	z.Sub(w, x)
 	z.Inv(z)
 	temp.Sub(v, x)
@@ -205,27 +204,27 @@ func (z *DualComplex) CrossRatio(v, w, x, y *DualComplex) *DualComplex {
 // Möbius sets z equal to the Möbius (fractional linear) transform of y:
 // 		(a*y + b) * Inv(c*y + d)
 // Then it returns z.
-func (z *DualComplex) Möbius(y, a, b, c, d *DualComplex) *DualComplex {
+func (z *DualPerplex) Möbius(y, a, b, c, d *DualPerplex) *DualPerplex {
 	z.Mul(a, y)
 	z.Add(z, b)
-	temp := new(DualComplex)
+	temp := new(DualPerplex)
 	temp.Mul(c, y)
 	temp.Add(temp, d)
 	temp.Inv(temp)
 	return z.Mul(z, temp)
 }
 
-// Generate returns a random DualComplex value for quick.Check testing.
-func (z *DualComplex) Generate(rand *rand.Rand, size int) reflect.Value {
-	randomDualComplex := &DualComplex{
-		*NewComplex(
+// Generate returns a random DualPerplex value for quick.Check testing.
+func (z *DualPerplex) Generate(rand *rand.Rand, size int) reflect.Value {
+	randomDualPerplex := &DualPerplex{
+		*NewPerplex(
 			big.NewRat(rand.Int63(), rand.Int63()),
 			big.NewRat(rand.Int63(), rand.Int63()),
 		),
-		*NewComplex(
+		*NewPerplex(
 			big.NewRat(rand.Int63(), rand.Int63()),
 			big.NewRat(rand.Int63(), rand.Int63()),
 		),
 	}
-	return reflect.ValueOf(randomDualComplex)
+	return reflect.ValueOf(randomDualPerplex)
 }
---
+++ dualperplex_test.go
@@ -0,0 +1,343 @@
+// Copyright (c) 2016 Melvin Eloy Irizarry-Gelpí
+// Licenced under the MIT License.
+
+package rational
+
+import (
+	"math/big"
+	"testing"
+	"testing/quick"
+)
+
+// Commutativity
+
+func TestDualPerplexAddCommutative(t *testing.T) {
+	f := func(x, y *DualPerplex) bool {
+		// t.Logf("x = %v, y = %v", x, y)
+		l := new(DualPerplex).Add(x, y)
+		r := new(DualPerplex).Add(y, x)
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestDualPerplexMulCommutative(t *testing.T) {
+	f := func(x, y *DualPerplex) bool {
+		// t.Logf("x = %v, y = %v", x, y)
+		l := new(DualPerplex).Mul(x, y)
+		r := new(DualPerplex).Mul(y, x)
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestDualPerplexNegConjCommutative(t *testing.T) {
+	f := func(x *DualPerplex) bool {
+		// t.Logf("x = %v", x)
+		l, r := new(DualPerplex), new(DualPerplex)
+		l.Neg(l.Conj(x))
+		r.Conj(r.Neg(x))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+// Anti-commutativity
+
+func TestDualPerplexSubAntiCommutative(t *testing.T) {
+	f := func(x, y *DualPerplex) bool {
+		// t.Logf("x = %v, y = %v", x, y)
+		l, r := new(DualPerplex), new(DualPerplex)
+		l.Sub(x, y)
+		r.Sub(y, x)
+		r.Neg(r)
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+// Associativity
+
+func TestDualPerplexAddAssociative(t *testing.T) {
+	f := func(x, y, z *DualPerplex) bool {
+		// t.Logf("x = %v, y = %v, z = %v", x, y, z)
+		l, r := new(DualPerplex), new(DualPerplex)
+		l.Add(l.Add(x, y), z)
+		r.Add(x, r.Add(y, z))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestDualPerplexMulAssociative(t *testing.T) {
+	f := func(x, y, z *DualPerplex) bool {
+		// t.Logf("x = %v, y = %v, z = %v", x, y, z)
+		l, r := new(DualPerplex), new(DualPerplex)
+		l.Mul(l.Mul(x, y), z)
+		r.Mul(x, r.Mul(y, z))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+// Identity
+
+func TestDualPerplexAddZero(t *testing.T) {
+	zero := new(DualPerplex)
+	f := func(x *DualPerplex) bool {
+		// t.Logf("x = %v", x)
+		l := new(DualPerplex).Add(x, zero)
+		return l.Equals(x)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestDualPerplexMulOne(t *testing.T) {
+	one := &Perplex{
+		l: *big.NewRat(1, 1),
+	}
+	zero := new(Perplex)
+	f := func(x *DualPerplex) bool {
+		// t.Logf("x = %v", x)
+		l := new(DualPerplex).Mul(x, &DualPerplex{*one, *zero})
+		return l.Equals(x)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestDualPerplexMulInvOne(t *testing.T) {
+	one := &Perplex{
+		l: *big.NewRat(1, 1),
+	}
+	zero := new(Perplex)
+	f := func(x *DualPerplex) bool {
+		// t.Logf("x = %v", x)
+		l := new(DualPerplex)
+		l.Mul(x, l.Inv(x))
+		return l.Equals(&DualPerplex{*one, *zero})
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestDualPerplexAddNegSub(t *testing.T) {
+	f := func(x, y *DualPerplex) bool {
+		// t.Logf("x = %v, y = %v", x, y)
+		l, r := new(DualPerplex), new(DualPerplex)
+		l.Sub(x, y)
+		r.Add(x, r.Neg(y))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestDualPerplexAddScalDouble(t *testing.T) {
+	f := func(x *DualPerplex) bool {
+		// t.Logf("x = %v", x)
+		l, r := new(DualPerplex), new(DualPerplex)
+		l.Add(x, x)
+		r.Scal(x, big.NewRat(2, 1))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+// Involutivity
+
+func TestDualPerplexInvInvolutive(t *testing.T) {
+	f := func(x *DualPerplex) bool {
+		// t.Logf("x = %v", x)
+		l := new(DualPerplex)
+		l.Inv(l.Inv(x))
+		return l.Equals(x)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestDualPerplexNegInvolutive(t *testing.T) {
+	f := func(x *DualPerplex) bool {
+		// t.Logf("x = %v", x)
+		l := new(DualPerplex)
+		l.Neg(l.Neg(x))
+		return l.Equals(x)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestDualPerplexConjInvolutive(t *testing.T) {
+	f := func(x *DualPerplex) bool {
+		// t.Logf("x = %v", x)
+		l := new(DualPerplex)
+		l.Conj(l.Conj(x))
+		return l.Equals(x)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+// Anti-distributivity
+
+func TestDualPerplexMulConjAntiDistributive(t *testing.T) {
+	f := func(x, y *DualPerplex) bool {
+		// t.Logf("x = %v, y = %v", x, y)
+		l, r := new(DualPerplex), new(DualPerplex)
+		l.Conj(l.Mul(x, y))
+		r.Mul(r.Conj(y), new(DualPerplex).Conj(x))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestDualPerplexMulInvAntiDistributive(t *testing.T) {
+	f := func(x, y *DualPerplex) bool {
+		// t.Logf("x = %v, y = %v", x, y)
+		l, r := new(DualPerplex), new(DualPerplex)
+		l.Inv(l.Mul(x, y))
+		r.Mul(r.Inv(y), new(DualPerplex).Inv(x))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+// Distributivity
+
+func TestDualPerplexAddConjDistributive(t *testing.T) {
+	f := func(x, y *DualPerplex) bool {
+		// t.Logf("x = %v, y = %v", x, y)
+		l, r := new(DualPerplex), new(DualPerplex)
+		l.Add(x, y)
+		l.Conj(l)
+		r.Add(r.Conj(x), new(DualPerplex).Conj(y))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestDualPerplexSubConjDistributive(t *testing.T) {
+	f := func(x, y *DualPerplex) bool {
+		// t.Logf("x = %v, y = %v", x, y)
+		l, r := new(DualPerplex), new(DualPerplex)
+		l.Sub(x, y)
+		l.Conj(l)
+		r.Sub(r.Conj(x), new(DualPerplex).Conj(y))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestDualPerplexAddScalDistributive(t *testing.T) {
+	f := func(x, y *DualPerplex) bool {
+		// t.Logf("x = %v, y = %v", x, y)
+		a := big.NewRat(2, 1)
+		l, r := new(DualPerplex), new(DualPerplex)
+		l.Scal(l.Add(x, y), a)
+		r.Add(r.Scal(x, a), new(DualPerplex).Scal(y, a))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestDualPerplexSubScalDistributive(t *testing.T) {
+	f := func(x, y *DualPerplex) bool {
+		// t.Logf("x = %v, y = %v", x, y)
+		a := big.NewRat(2, 1)
+		l, r := new(DualPerplex), new(DualPerplex)
+		l.Scal(l.Sub(x, y), a)
+		r.Sub(r.Scal(x, a), new(DualPerplex).Scal(y, a))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestDualPerplexAddMulDistributive(t *testing.T) {
+	f := func(x, y, z *DualPerplex) bool {
+		// t.Logf("x = %v, y = %v, z = %v", x, y, z)
+		l, r := new(DualPerplex), new(DualPerplex)
+		l.Mul(l.Add(x, y), z)
+		r.Add(r.Mul(x, z), new(DualPerplex).Mul(y, z))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestDualPerplexSubMulDistributive(t *testing.T) {
+	f := func(x, y, z *DualPerplex) bool {
+		// t.Logf("x = %v, y = %v, z = %v", x, y, z)
+		l, r := new(DualPerplex), new(DualPerplex)
+		l.Mul(l.Sub(x, y), z)
+		r.Sub(r.Mul(x, z), new(DualPerplex).Mul(y, z))
+		return l.Equals(r)
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+// Positivity
+
+func TestDualPerplexQuadPositive(t *testing.T) {
+	f := func(x *DualPerplex) bool {
+		// t.Logf("x = %v", x)
+		return x.Quad().Sign() > 0
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+// Composition
+
+func TestDualPerplexComposition(t *testing.T) {
+	f := func(x, y *DualPerplex) bool {
+		// t.Logf("x = %v, y = %v", x, y)
+		p := new(DualPerplex)
+		a, b := new(big.Rat), new(big.Rat)
+		p.Mul(x, y)
+		a.Set(p.Quad())
+		b.Mul(x.Quad(), y.Quad())
+		return a.Cmp(b) == 0
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
